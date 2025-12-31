10fc67773ce7b867be9d0f66ed7278de2eb5e280
Cosmo <cmyk05@gmail.com>
2025-12-30 19:05:57 -0800

    Fixed another small issue with weather_fetch.py

--- scripts/weather_fetch.py
+++ scripts/weather_fetch.py
@@ -4,8 +4,9 @@ import requests
 download_url = """https://api.open-meteo.com/v1/forecast?latitude=49.265&longitude=-123.242&
                 daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&hourly=temperature_2m,
                 apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,
-                snow_depth&models=best_match&current=temperature_2m,relative_humidity_2m,apparent_temperature,
-                precipitation,rain,showers,snowfall,cloud_cover,wind_speed_10m&timezone=America%2FLos_Angeles&format=csv"""
+                snow_depth&models=best_match&current=temperature_2m,relative_humidity_2m,
+                apparent_temperature,precipitation,rain,showers,snowfall,cloud_cover,
+                wind_speed_10m&timezone=America%2FLos_Angeles&format=csv"""
 
 # Make the API call
 response = requests.get(download_url, headers={}, stream=True)
@@ -20,3 +21,4 @@ else:
     print(f"Failed to download. Status code: {response.status_code}")
     print("Response content (first 500 chars):")
     print(response.text[:500])
+
